a78e5cd95db923aef3febc2e1503c3bad64fc4bd
arthursmelkovs92 <arthursmelkovs@outlook.com>
2026-03-03 11:54:26 +0000

    Updated Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -27,7 +27,11 @@ const skills: SkillCategory[] = [
     items: [
       "Usage of RITA to analyze and detect malicious network traffic",
       "Usage of Wireshark for network traffic capturing",
-      "IDS/IPS snort"
+      "IDS/IPS snort",
+      "SIEM Platforms: Hands-on experience with Microsoft Sentinel, Splunk, and IBM QRadar for threat detection and response",
+      "Log Analysis and Correlation",
+      "Real-time Alerting and Threat Intelligence Integration",
+      "Cloud Security Monitoring"
     ]
   },
   {
